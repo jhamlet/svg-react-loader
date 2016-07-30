c1052d5d9a093358acecb85ffe71ea21fe9242b4
Jerry Hamlet <jerry@hamletink.com>
2016-07-30 16:13:04 -0700

    Update loader to pass the correct prefix

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,12 +1,33 @@
 /*globals describe, it*/
+import R from 'ramda';
 import React, { Component } from 'react';
-import SimpleSvg from '../../lib/loader.js?name=SimpleSvg!../samples/simple.svg';
 import { mount } from 'enzyme';
+
+import SimpleSvg from '../../lib/loader.js?name=SimpleSvg!../samples/simple.svg';
+import StylesSvg from '../../lib/loader.js?classIdPrefix!../samples/styles.svg';
+import TextSvg from '../../lib/loader.js!../samples/text.svg';
 
 require('should');
 
+const getPropsMinusChildren = R.pipe(
+    R.invoker(0, 'props'),
+    R.omit(['children'])
+);
+
 describe('svg-react-loader', () => {
+
+    const expectedSvgProps = {
+        version:          "1.1",
+        x:                "0px",
+        y:                "0px",
+        viewBox:          "0 0 16 16",
+        enableBackground: "new 0 0 16 16",
+        xmlSpace:         "preserve",
+        className:        "simple"
+    };
+
     it('simple.svg', () => {
+
         class Icon extends Component {
             render () {
                 return <SimpleSvg />;
@@ -14,16 +35,6 @@ describe('svg-react-loader', () => {
         };
 
         const wrapper = mount(<Icon />);
-
-        const expectedSvgProps = {
-            version:          "1.1",
-            x:                "0px",
-            y:                "0px",
-            viewBox:          "0 0 16 16",
-            enableBackground: "new 0 0 16 16",
-            xmlSpace:         "preserve",
-            className:        "simple"
-        };
 
         wrapper.
             containsMatchingElement(<svg {...expectedSvgProps} />).
@@ -39,6 +50,66 @@ describe('svg-react-loader', () => {
 
         wrapper.
             containsMatchingElement(<text>Foobar</text>).
+            should.
+            be.
+            true;
+    });
+
+    it('styles.svg', () => {
+        const wrapper = mount(<StylesSvg />);
+
+        const expectedProps = {
+            version: "1.1",
+            id: "Layer_1",
+            width: "50px",
+            height: "50px",
+            x: "0px",
+            y: "0px",
+            viewBox: "0 0 50 50",
+            style: {
+                enableBackground: 'new 0 0 50 50'
+            },
+            xmlSpace: "preserve",
+            preserveAspectRatio: "none"
+        };
+
+        getPropsMinusChildren(wrapper.find('svg')).
+            should.
+            eql(expectedProps);
+
+        wrapper.
+            containsMatchingElement(<svg {...expectedProps} />).
+            should.
+            be.
+            true;
+
+        wrapper.
+            childAt(0).
+            text().
+            should.
+            equal(
+                ".Styles__st0{fill-rule:evenodd;clip-rule:evenodd;fill:#B2B2B2;}" +
+                ".Styles__foo{background:url('foo/bar/baz.jpg');}"
+            );
+    });
+
+    it('text.svg', () => {
+        const wrapper = mount(<TextSvg />);
+
+        wrapper.
+            containsMatchingElement(<svg />).
+            should.
+            be.
+            true;
+
+        wrapper.
+            containsMatchingElement(<title>The Title</title>).
+            should.
+            be.
+            true;
+
+        wrapper.
+            containsMatchingElement(<text x="20" y="20">Text</text>).
             should.
             be.
             true;
