101e04fccc6e40c4e6178e64f6c32abd3c787e58
Jerry Hamlet <jerry@hamletink.com>
2017-06-15 06:46:05 -0700

    Bring up to date with webpack and babel

--- karma.conf.js
+++ karma.conf.js
@@ -18,10 +18,7 @@ module.exports = function (config) {
                     {
                         test: /\.js$/,
                         exclude: /node_modules/,
-                        loader: 'babel',
-                        query: {
-                            presets: ['es2015', 'react']
-                        }
+                        loader: 'babel-loader'
                     }
                 ]
            },
