7fd91eb58b2c5cbd9e1a4dd7ae16e6f7683c0245
Jerry Hamlet <jerry@hamletink.com>
2017-06-14 21:31:19 -0700

    Update package versions and modify test script

--- bin/test.sh
+++ bin/test.sh
@@ -9,29 +9,6 @@ if [ $TYPE == 'karma' ]
 then
     $KARMA start
 else
-    $MOCHA \
-        --harmony \
-        --harmony_shipping \
-        --harmony_modules \
-        --harmony_array_includes \
-        --harmony_regexps \
-        --harmony_proxies \
-        --harmony_sloppy \
-        --harmony_unicode_regexps \
-        --harmony_reflect \
-        --harmony_destructuring \
-        --harmony_sharedarraybuffer \
-        --harmony_atomics \
-        --harmony_new_target \
-        --harmony_tostring \
-        --harmony_concat_spreadable \
-        --harmony_rest_parameters \
-        --harmony_spreadcalls \
-        --harmony_spread_arrays \
-        --harmony_arrow_functions \
-        --harmony_computed_property_names \
-        --harmony_unicode \
-        --harmony_object \
-        $(find $DIR -type f -name '*.js')
+    $MOCHA $(find $DIR -type f -name '*.js')
 fi
 
