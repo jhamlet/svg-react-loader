1cf6149d7193cae50821763bb60263bcd1491289
Bill Brinkert <william.brinkert@akqa.com>
2018-08-21 14:11:13 -0700

    feat: add simple tests

---
+++ test/unit/sanitize/filters/unique-id-prefix.js
@@ -0,0 +1,56 @@
+/*globals describe, it*/
+require('should');
+
+describe('svg-react-loader/lib/sanitize/filters/prefix-style-class-id', () => {
+  const traverse = require('traverse');
+  const prefixStyleClassnames =
+    require('../../../../lib/sanitize/filters/unique-svg-ids')({prefix: 'svgFilename__'});
+
+  it('should work on a simple tree', () => {
+    const tree = {
+      id: 'a',
+      fill: 'url(#a)',
+      mask: 'url(#a)',
+      xlinkHref: '#a'
+    };
+
+    var result = traverse.map(tree, prefixStyleClassnames);
+
+    result.
+      should.
+      eql({
+        id: 'svgFilename__a',
+        fill: 'url(#svgFilename__a)',
+        mask: 'url(#svgFilename__a)',
+        xlinkHref: '#svgFilename__a'
+      });
+  });
+
+  it('should work on a more complex tree', () => {
+    const tree = {
+      id: 'a',
+      id: 'b',
+      fill: 'url(#a)',
+      mask: 'url(#a)',
+      xlinkHref: '#a',
+      fill: 'url(#b)',
+      mask: 'url(#b)',
+      xlinkHref: '#b'
+    };
+
+    const result = traverse.map(tree, prefixStyleClassnames);
+
+    result.
+      should.
+      eql({
+        id: 'svgFilename__a',
+        fill: 'url(#svgFilename__a)',
+        mask: 'url(#svgFilename__a)',
+        xlinkHref: '#svgFilename__a',
+        id: 'svgFilename__b',
+        fill: 'url(#svgFilename__b)',
+        mask: 'url(#svgFilename__b)',
+        xlinkHref: '#svgFilename__b'
+      });
+  });
+});
